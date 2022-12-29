511e0740be9e9257812e43f980889f6eb8cd1fdd
anti_pode <ts@paraweb.me>
2022-12-29 21:12:10 +0700

    feat: init commit

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,14 @@
 import { defineComponent, onMounted, ref } from "vue";
 
 import AppCalendar from "@/components/Calendar/Calendar.vue";
-import AppSection from "@/components/Section/Section.vue";
-import AppButton from "@/components/UI/Button.vue";
-import AppHeader from "@/components/Section/Header.vue";
+import AppPlayer from "@/components/UI/Player.vue";
 
 // import mockItems from "@/api/items";
 import type { IItem } from "@/api/items.types";
 
 export default defineComponent({
-  components: { AppButton, AppSection, AppCalendar, AppHeader },
+  components: { AppCalendar, AppPlayer },
   setup() {
-    const { VITE_SUPPORT_LINK } = import.meta.env;
     const items = ref<IItem[] | null>(null);
 
     onMounted(async () => {
@@ -27,7 +24,6 @@ export default defineComponent({
 
     return {
       items,
-      VITE_SUPPORT_LINK,
     };
   },
 });
@@ -35,39 +31,26 @@ export default defineComponent({
 
 <template>
   <div id="#app">
-    <AppHeader />
-
     <section class="container">
-      <AppSection title="Адвент-календарь добрых дел">
-        <p>
-          <b>Дорогой друг! Поздравляем вас с&nbsp;Новым годом и&nbsp;Рождеством!</b>
-        </p>
-        <p>
-          Наш фонд помогает детям, оказавшимся в&nbsp;сложной жизненной ситуации, в&nbsp;сиротских учреждениях, кровных
-          и&nbsp;приемных семьях. Мы&nbsp;верим, что благодаря участию неравнодушных людей мы&nbsp;можем изменить судьбы
-          этих детей к&nbsp;лучшему. Наша миссия&nbsp;&mdash; сделать все, чтобы дети жили в&nbsp;семье,
-          а&nbsp;не&nbsp;в&nbsp;детских домах.
-        </p>
-        <p>
-          Каждый день нашего адвент-календаря посвящен добрым делам. Вы&nbsp;узнаете о том, что нам уже удалось сделать
-          в&nbsp;этом году благодаря вашей поддержке, как можно помочь фонду, какие дела могут стать полезными
-          привычками. А еще мы&nbsp;приготовили для вас небольшие подарки.
-        </p>
-        <p>
-          Открывайте окошки нашего календаря, делитесь историями, и&nbsp;пусть уходящий год для каждого из&nbsp;нас
-          станет немного теплее.
-        </p>
-      </AppSection>
+      <div class="container__content">
+        <h1 class="container__title">Поздравляем вас с новым <span>2023 годом!</span></h1>
+
+        <div class="container__description">
+          Тут будет краткий вводный текст про поздравление от&nbsp;Лены и&nbsp;общие слова про новый год.
+          И&nbsp;указание что ниже - кнопка для получение подарочка.
+        </div>
+
+        <AppPlayer src="/src/assets/media/1.mp4" />
+      </div>
 
       <AppCalendar v-if="items && items.length > 0" :items="items" />
-
-      <AppButton text="Поддержать фонд" :href="VITE_SUPPORT_LINK" />
     </section>
   </div>
 </template>
 
 <style lang="scss">
 @import "@/assets/styles/breakpoints";
+@import "@/assets/styles/fonts";
 
 #app {
   overflow: hidden;
@@ -78,11 +61,29 @@ export default defineComponent({
   }
 
   .container {
-    > .btn {
-      position: relative;
-      z-index: 3;
-      display: table;
-      margin: 0 auto;
+    &__content {
+      max-width: 722px;
+    }
+
+    &__title {
+      font-family: $font-title;
+      font-weight: 700;
+      font-size: 72px;
+      line-height: 100%;
+
+      span {
+        font-family: $font-italic;
+        font-weight: 300;
+      }
+    }
+
+    &__description {
+      margin-top: 10px;
+      margin-bottom: 37px;
+      font-family: $font-title;
+      font-weight: 500;
+      font-size: 18px;
+      line-height: 20px;
     }
   }
 }
---
+++ src/components/UI/Player.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AppPlayer",
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {},
+});
+</script>
+
+<template>
+  <div class="video">
+    <video :src="src" controls autoplay loop></video>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.video {
+  width: 100%;
+
+  video {
+    width: 100%;
+  }
+}
+</style>
